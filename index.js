8d733b0a632484edd3002846511f3a5815621b1d
viro <twikista@gmail.com>
2023-04-30 05:20:20 +0100

    extracted request routes to a seperate module

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./model/blog");
+const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blogs");
 
 const uri =
   "mongodb+srv://twikista:throwaykey007@cluster0.cxggszr.mongodb.net/bloglist?retryWrites=true&w=majority";
@@ -13,19 +14,20 @@ mongoose.connect(uri).then(() => {
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+// app.get("/api/blogs", (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs);
+//   });
+// });
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog({ ...request.body });
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+// app.post("/api/blogs", (request, response) => {
+//   const blog = new Blog({ ...request.body });
+//   blog.save().then((result) => {
+//     response.status(201).json(result);
+//   });
+// });
 
 const PORT = 3001;
 app.listen(PORT, () => {
