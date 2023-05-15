774d5262f18b8fbed35e8ef36e3534580a9c7b50
viro <twikista@gmail.com>
2023-05-15 08:23:51 +0100

    extracted reusable code into seperate module - test_helper

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -1,42 +1,43 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const testHelper = require("./test_helper");
 const app = require("../app");
 
 const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-  {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-  },
-];
+// const initialBlogs = [
+//   {
+//     title: "React patterns",
+//     author: "Michael Chan",
+//     url: "https://reactpatterns.com/",
+//     likes: 7,
+//   },
+//   {
+//     title: "Go To Statement Considered Harmful",
+//     author: "Edsger W. Dijkstra",
+//     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+//     likes: 5,
+//   },
+//   {
+//     title: "First class tests",
+//     author: "Robert C. Martin",
+//     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+//     likes: 10,
+//   },
+// ];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(testHelper.initialBlogs[0]);
   await blogObject.save();
 
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(testHelper.initialBlogs[1]);
   await blogObject.save();
 
-  blogObject = new Blog(initialBlogs[2]);
+  blogObject = new Blog(testHelper.initialBlogs[2]);
   await blogObject.save();
 }, 100000);
 
@@ -47,11 +48,11 @@ beforeEach(async () => {
 //     .expect("Content-Type", /application\/json/);
 // });
 
-test("there 3 blogs in JSON format", async () => {
+test("there are 3 blogs in JSON format", async () => {
   const response = await api.get("/api/blogs");
   console.log(response.body.length);
 
-  expect(response.body.length).toBe(3);
+  expect(response.body.length).toBe(testHelper.initialBlogs.length);
   expect(response.headers["content-type"].split(";")).toContain(
     "application/json"
   );
@@ -77,10 +78,10 @@ test("valid blog can be added", async () => {
     .expect(201)
     .expect("Content-Type", /application\/json/);
 
-  const response = await api.get("/api/blogs");
-  const authors = response.body.map((i) => i.author);
+  const blogs = await testHelper.blogsInDB();
+  const authors = blogs.map((i) => i.author);
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  expect(blogs).toHaveLength(testHelper.initialBlogs.length + 1);
   expect(authors).toContain("Pascal Milton");
 });
 
@@ -97,9 +98,9 @@ test("set likes property to Zero if missing from request", async () => {
     .expect(201)
     .expect("Content-Type", /application\/json/);
 
-  const response = await api.get("/api/blogs");
-  const lastBlogAdded = response.body.at(-1);
-  console.log(lastBlogAdded);
+  const blogs = await testHelper.blogsInDB();
+  const lastBlogAdded = blogs.at(-1);
+  // console.log(lastBlogAdded);
   expect(lastBlogAdded.likes).toBe(0);
 });
 
@@ -111,9 +112,9 @@ test("blog without title is not added", async () => {
   };
   await api.post("/api/blogs").send(newBlog).expect(400);
 
-  const response = await api.get("/api/blogs");
+  const blogs = await testHelper.blogsInDB();
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+  expect(blogs).toHaveLength(testHelper.initialBlogs.length);
 });
 
 test("blog without url is not added", async () => {
@@ -125,8 +126,8 @@ test("blog without url is not added", async () => {
 
   await api.post("/api/blogs").send(newBlog).expect(400);
 
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(initialBlogs.length);
+  const blogs = await testHelper.blogsInDB();
+  expect(blogs).toHaveLength(testHelper.initialBlogs.length);
 });
 
 afterAll(async () => {
---
+++ tests/test_helper.js
@@ -0,0 +1,32 @@
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  },
+];
+
+const blogsInDB = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map((i) => i.toJSON());
+};
+
+module.exports = {
+  initialBlogs,
+  blogsInDB,
+};
