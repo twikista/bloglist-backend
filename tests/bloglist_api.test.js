a199a274f6a7afd777160ee4e7c31bdee471b9d3
viro <twikista@gmail.com>
2023-05-22 21:11:34 +0100

    refactored tests for authentication

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -8,7 +8,7 @@ const api = supertest(app);
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-describe("user must be authenticated", () => {
+describe("blog route is protected", () => {
   let auth = {};
   beforeAll(async () => {
     const credentials = { username: "fingy", password: "123456" };
@@ -19,11 +19,9 @@ describe("user must be authenticated", () => {
       .expect("Content-Type", /application\/json/);
 
     auth.token = response.body.token;
-    console.log(auth);
   }, 100000);
 
   beforeEach(async () => {
-    console.log(auth);
     await Blog.deleteMany({});
 
     const blogObjects = testHelper.initialBlogs.map((blog) => new Blog(blog));
@@ -31,48 +29,157 @@ describe("user must be authenticated", () => {
     await Promise.all(promiseArray);
   }, 100000);
 
-  test("to view all blogs", async () => {
-    const blogsAtStart = await testHelper.blogsInDB();
-    console.log(blogsAtStart);
+  describe("all blogs", () => {
+    test("can be viewed by authenticated user", async () => {
+      const blogsAtStart = await testHelper.blogsInDB();
 
-    const response = api
-      .get("/api/blogs")
-      .set("Authorization", `Bearer ${auth.token}`)
-      .expect(200)
-      .expect("Content-Type", /application\/json/);
-    console.log(response);
-    expect(response.body).toHaveLength(blogsAtStart.length);
-  });
-});
+      const response = await api
+        .get("/api/blogs")
+        .set("Authorization", `Bearer ${auth.token}`)
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+      expect(response.body).toHaveLength(blogsAtStart.length);
+    });
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-
-  const blogObjects = testHelper.initialBlogs.map((blog) => new Blog(blog));
-  const promiseArray = blogObjects.map((i) => i.save());
-  await Promise.all(promiseArray);
-}, 100000);
-
-describe("with some blogs already saved", () => {
-  test("all blogs are returned in JSON format", async () => {
-    const response = await api.get("/api/blogs");
-    console.log(response.body.length);
-
-    expect(response.body.length).toBe(testHelper.initialBlogs.length);
-    expect(response.headers["content-type"].split(";")).toContain(
-      "application/json"
-    );
-  });
-});
-
-describe("viewing a specific blog", () => {
-  test("must have uniquie identifier named 'id'", async () => {
-    const response = await api.get("/api/blogs");
-    response.body.forEach((item) => {
-      expect(item.id).toBeDefined();
+    test("viewed by authenticated user are returned in JSON format", async () => {
+      await api
+        .get("/api/blogs")
+        .set("Authorization", `Bearer ${auth.token}`)
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
     });
   });
+
+  describe("single blog", () => {
+    test("viewed by authenticated user must have uniquie identifier named 'id'", async () => {
+      const response = await api
+        .get("/api/blogs")
+        .set("Authorization", `Bearer ${auth.token}`)
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+      response.body.forEach((item) => {
+        expect(item.id).toBeDefined();
+      });
+    });
+  });
+
+  describe("adding a blog", () => {
+    test("is successful if user is authenticated", async () => {
+      const newBlog = {
+        title: "First class tests",
+        author: "Pascal Milton",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+      };
+      await api
+        .post("/api/blogs")
+        .set("Authorization", `Bearer ${auth.token}`)
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+      const blogs = await testHelper.blogsInDB();
+      const authors = blogs.map((i) => i.author);
+
+      expect(blogs).toHaveLength(testHelper.initialBlogs.length + 1);
+      expect(authors).toContain("Pascal Milton");
+    });
+
+    test("sets 'likes' property to Zero if not specified", async () => {
+      const newBlog = {
+        title: "Test for likes property",
+        author: "Fullstack Open",
+        url: "http://blog.viro.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      };
+
+      await api
+        .post("/api/blogs")
+        .set("Authorization", `Bearer ${auth.token}`)
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+      const blogs = await testHelper.blogsInDB();
+      const lastBlogAdded = blogs.at(-1);
+      expect(lastBlogAdded.likes).toBe(0);
+    });
+
+    test("fails with status code 400 if title is not specified", async () => {
+      const newBlog = {
+        author: "Golon",
+        url: "http://blog.viro.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 8,
+      };
+      await api
+        .post("/api/blogs")
+        .set("AUthorization", `Bearer ${auth.token}`)
+        .send(newBlog)
+        .expect(400);
+
+      const blogs = await testHelper.blogsInDB();
+
+      expect(blogs).toHaveLength(testHelper.initialBlogs.length);
+    });
+  });
+
+  //   test("a specifmust have uniquie identifier named 'id'", async () => {
+  //     const response = await api.get("/api/blogs");
+  //     response.body.forEach((item) => {
+  //       expect(item.id).toBeDefined();
+  //     });
+  //   });
+  // });
+
+  // test("authenticated user can add a blog", async () => {
+  //   const newBlog = {
+  //     title: "First class tests",
+  //     author: "Pascal Milton",
+  //     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+  //     likes: 10,
+  //   };
+  //   await api
+  //     .post("/api/blogs")
+  //     .set("Authorization", `Bearer ${auth.token}`)
+  //     .send(newBlog)
+  //     .expect(201)
+  //     .expect("Content-Type", /application\/json/);
+
+  //   const blogs = await testHelper.blogsInDB();
+  //   const authors = blogs.map((i) => i.author);
+
+  //   expect(blogs).toHaveLength(testHelper.initialBlogs.length + 1);
+  //   expect(authors).toContain("Pascal Milton");
+  // });
 });
+
+// beforeEach(async () => {
+//   await Blog.deleteMany({});
+
+//   const blogObjects = testHelper.initialBlogs.map((blog) => new Blog(blog));
+//   const promiseArray = blogObjects.map((i) => i.save());
+//   await Promise.all(promiseArray);
+// }, 100000);
+
+// describe("with some blogs already saved", () => {
+//   test("all blogs are returned in JSON format", async () => {
+//     const response = await api.get("/api/blogs");
+//     console.log(response.body.length);
+
+//     expect(response.body.length).toBe(testHelper.initialBlogs.length);
+//     expect(response.headers["content-type"].split(";")).toContain(
+//       "application/json"
+//     );
+//   });
+// });
+
+// describe("viewing a specific blog", () => {
+//   test("must have uniquie identifier named 'id'", async () => {
+//     const response = await api.get("/api/blogs");
+//     response.body.forEach((item) => {
+//       expect(item.id).toBeDefined();
+//     });
+//   });
+// });
 
 describe("addition of a new blog", () => {
   test("succeeds with valid data", async () => {
