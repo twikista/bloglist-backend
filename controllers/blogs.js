831ee5878a758d21e0d0d65a3da02afd9e607a72
Aaron Anama <twikista@gmail.com>
2023-08-31 05:41:08 +0100

    refactored blog controller to handle comments

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,14 +12,15 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogRouter.get('/:id', async (request, response) => {
+  const blogId = request.params.id
+  const blog = await Blog.findById(blogId)
+    .populate('user', { username: 1, name: 1 })
+    .populate('comments')
+  response.status(200).json(blog)
+})
+
 blogRouter.post('/', middleware.tokenExtractor, async (request, response) => {
-  // console.log("i am starting");
-  // const token = jwt.verify(request.token, process.env.SECRET);
-  // if (!token.id) {
-  //   return response.status(401).json({ error: "invalid token" });
-  // }
-  // console.log(token);
-  // const { id } = token;
   const user = request.user
 
   const blog = request.body.likes
@@ -37,16 +38,19 @@ blogRouter.delete(
   '/:id',
   middleware.tokenExtractor,
   async (request, response) => {
-    // const token = jwt.verify(request.token, process.env.SECRET);
-    // if (!token.id) {
-    //   return response.status(401).json({ error: "invalid token" });
-    // }
     const user = request.user
 
     const blogId = request.params.id
     const blog = await Blog.findById(blogId)
     if (blog.user.toString() === user.id.toString()) {
       await Blog.findByIdAndRemove(blogId)
+      console.log(`user id: ${user.id}`)
+      console.log(`blog id: ${blog._id}`)
+      User.updateOne(
+        {},
+        { $pull: { blogs: blog._id.toString() } },
+        { multi: true }
+      )
       response.status(204).end()
     } else {
       return response.status(403).json({ error: 'unauthorized' })
@@ -54,26 +58,35 @@ blogRouter.delete(
   }
 )
 
-blogRouter.put('/:id', middleware.tokenExtractor, async (request, response) => {
+blogRouter.put('/:id', async (request, response) => {
   const id = request.params.id
   const blogUpdateData = request.body
 
-  const user = request.user
-  const blog = await Blog.findById(id)
+  // const user = request.user
+  // const blog = await Blog.findById(id)
 
-  if (user.id.toString() === blog.user.toString()) {
-    const updatedBlog = await Blog.findByIdAndUpdate(id, blogUpdateData, {
-      new: true,
-    })
-    response.status(200).json(updatedBlog)
-  } else {
-    return response.status(403).json({ error: 'unauthorized' })
+  // if (user.id.toString() === blog.user.toString()) {
+  //   const updatedBlog = await Blog.findByIdAndUpdate(id, blogUpdateData, {
+  //     new: true,
+  //   })
+  //   response.status(200).json(updatedBlog)
+  // } else {
+  //   return response.status(403).json({ error: 'unauthorized' })
+  // }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blogUpdateData, {
+    new: true,
+  })
+  if (!updatedBlog) {
+    return response
+      .status(500)
+      .json({ error: 'request could not be processed' })
   }
+  response.status(200).json(updatedBlog)
 })
 
 blogRouter.post('/:id/comments', async (request, response) => {
   const blogId = request.params.id
-  console.log(blogId)
   const commentText = request.body.text
 
   const comment = new Comment({
@@ -83,7 +96,7 @@ blogRouter.post('/:id/comments', async (request, response) => {
   const savedComment = await comment.save()
 
   const blogCommentedOn = await Blog.findById(blogId)
-  blogCommentedOn.comments = blogCommentedOn.comments.concat(savedComment)
+  blogCommentedOn.comments = blogCommentedOn.comments.concat(savedComment._id)
   await blogCommentedOn.save()
 
   response.status(201).json(savedComment)
